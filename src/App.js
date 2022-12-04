a26daa43773a6e022b450eac00e3f323d53226e0
Pierrick Butin <pierrick.butin.v2@gmail.com>
2022-12-04 11:56:24 +0100

    Day4

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import Day1 from './days/Day1'
 import Day2 from './days/Day2'
 import Day3 from './days/Day3'
+import Day4 from './days/Day4'
 function App() {
   return (
     <div className="App">
-      <Day3></Day3>
+      <Day4></Day4>
     </div>
   );
 }
